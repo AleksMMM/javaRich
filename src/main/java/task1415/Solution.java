b445654952867e7bd8c827ad2877fc25c81f2bad
aleks <gol8an@yandex.ru>
2022-11-02 22:45:00 +0300

    Клининговый центр

---
+++ src/main/java/task1415/Solution.java
@@ -0,0 +1,55 @@
+package task1415;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/* 
+Клининговый центр
+*/
+
+public class Solution {
+    public static void main(String[] args) {
+        List<Apartment> apartments = new ArrayList<>();
+        apartments.add(new OneRoomApt());
+        apartments.add(new TwoRoomApt());
+        apartments.add(new ThreeRoomApt());
+
+        cleanAllApartments(apartments);
+    }
+
+    public static void cleanAllApartments(List<Apartment> apartments) {
+        //написать тут вашу реализацию пунктов 1-4
+        apartments.forEach(
+                v -> {
+                    if (v instanceof OneRoomApt) {
+                        ((OneRoomApt) v).clean1Room();
+                    } else if (v instanceof TwoRoomApt) {
+                        ((TwoRoomApt) v).clean2Rooms();
+                    } else if (v instanceof ThreeRoomApt) {
+                        ((ThreeRoomApt) v).clean3Rooms();
+                    }
+                }
+        );
+    }
+
+    static interface Apartment {
+    }
+
+    static class OneRoomApt implements Apartment {
+        void clean1Room() {
+            System.out.println("1 room is cleaned");
+        }
+    }
+
+    static class TwoRoomApt implements Apartment {
+        void clean2Rooms() {
+            System.out.println("2 rooms are cleaned");
+        }
+    }
+
+    static class ThreeRoomApt implements Apartment {
+        void clean3Rooms() {
+            System.out.println("3 rooms are cleaned");
+        }
+    }
+}
